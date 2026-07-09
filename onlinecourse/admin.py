9164119b17b07fc6851ac3574a8b403fb212be78
karthikeya-2002 <karthikbhat44123@gmail.com>
2026-07-10 02:03:38 +0530

    Task 2: Configured admin.py with inlines and classes

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,39 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+# 1. Seven imported classes:
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# 2. Implementations of ChoiceInline and QuestionInline
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
-
-# Register your models here.
+# 3. Implementations of Admins
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text']
 
-# <HINT> Register Question and Choice models here
-
+# Registering all models so they appear on the admin site
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
