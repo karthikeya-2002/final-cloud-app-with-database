28a1884d8a50a0015b7b78f61062e24d44fee5f1
karthikeya-2002 <karthikbhat44123@gmail.com>
2026-07-09 20:18:28 +0530

    Task 1: Added Question, Choice, Submission models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -132,3 +132,27 @@ class Enrollment(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
+
+
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE) 
+    question_text = models.CharField(max_length=250)
+    grade = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=250)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.choice_text
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+
+    def __str__(self):
+        return f"Submission ID: {self.id} for Enrollment: {self.enrollment.id}"
